1b54332d1db3293d1ec8756a12f500fb343c2855
Jeyaprabakar01 <ej.prabakar@gmail.com>
2021-09-19 18:34:08 +0530

    Push Error Solved

--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -141,7 +141,7 @@ dishRouter.route('/:dishId/comments')
     .then((dish) => {
         if (dish != null) {
             req.body.author = req.user._id;
-            dish.comments.push(req.body);
+            dish.comments.unshift(req.body);
             dish.save()
             .then((dish) => {
                 Dishes.findById(dish._id)
